dc1f95db887fd0e536defbae54934431d9951cb2
Marvin <107920676+ShivenBonazzo@users.noreply.github.com>
2025-12-02 12:55:55 +0100

    Add files via upload

---
+++ ft_lstadd_front.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rarriola <rarriola@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/02 10:58:10 by rarriola          #+#    #+#             */
+/*   Updated: 2025/12/02 12:55:14 by rarriola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (lst != NULL && new != NULL)
+	{
+		new->next = *lst;
+		*lst = new;
+	}
+}
+
+/*
+adds the node 'new' at the beginning of the list.
+*/
---
+++ ft_lstnew.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rarriola <rarriola@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/02 10:51:13 by rarriola          #+#    #+#             */
+/*   Updated: 2025/12/02 11:02:13 by rarriola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = (t_list *)malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	node ->content = content;
+	node ->next = NULL;
+	return (node);
+}
+
+/*
+allocates memory using malloc and returns a new node
+'content' initialized with the given parameter 'content'.
+'next' is initialized to NULL.
+*/
